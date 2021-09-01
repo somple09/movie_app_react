869e8b5d2ec3f5853e0fc304a6355e2855b8f7ec
simple <jklee4741@gmail.com>
2021-09-01 21:43:24 +0900

    Updates

---
+++ precache-manifest.2312cf72d819202e825ee4b6827ff0dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "378820ff94eddb6b14d29954ff710fd9",
+    "url": "/movie_app_react/index.html"
+  },
+  {
+    "revision": "1d5885fd7b51ca63c1db",
+    "url": "/movie_app_react/static/css/main.78b81203.chunk.css"
+  },
+  {
+    "revision": "3d1a58d62d4fba334d91",
+    "url": "/movie_app_react/static/js/2.4501569b.chunk.js"
+  },
+  {
+    "revision": "1d5885fd7b51ca63c1db",
+    "url": "/movie_app_react/static/js/main.90ccc8ae.chunk.js"
+  },
+  {
+    "revision": "e88e53e1a436651e148c",
+    "url": "/movie_app_react/static/js/runtime~main.21233f46.js"
+  }
+]);
